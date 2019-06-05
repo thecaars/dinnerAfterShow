740054fc9ed7968ddf1f0d6fadc199b66b8b41f6
Alice-Lau <alice.p.k.lau@gmail.com>
2019-06-05 16:41:05 -0400

    Rush and I changed the organization of the header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            userCountry: "",
+            userCity: ""
+        }
+    }
+
+    handleOnChange = (e) => {
+        if (e.target.id === "country") {
+            this.setState({
+                userCountry: e.target.value
+            })
+        } else if (e.target.id === "city") {
+            this.setState({
+                userCity: e.target.value
+            })
+        }
+    }
+
+    handleOnSubmit = (e) => {
+        e.preventDefault();
+        // const countryValue = this.state.userCountry;
+        // const cityValue = this.state.userCity;
+        this.props.submitForm(this.state.userCountry, this.state.userCity);
+
+    }
+
     render() {
         return (
             <header>
                 <h1>Dinner After Show</h1>
                 <p>Input your country and city to see what upcoming events are happening around you.</p>
 
-                <form onSubmit={this.props.submitForm}>
+                <form onSubmit={this.handleOnSubmit}>
                     <div className="input">
                         <div className="selectInput">
                             <label htmlFor="country"></label>
-                            <select id="country" name="country" onChange={this.props.handleUserCountryChange}>
-                                <option value="" readonly>Country</option>
+                            <select id="country" name="country" onChange={this.handleOnChange}>
+                                <option value="" readOnly>Country</option>
                                 <option value="CA">Canada</option>
                             </select>
                         </div>
 
                         <div className="textInput">
                             <label htmlFor="city"></label>
-                            <input id="city" type="text" onChange={this.props.handleUserCityChange} />
+                            <input id="city" type="text" onChange={this.handleOnChange} />
                         </div>
                     </div>
 
@@ -33,8 +62,6 @@ class Header extends Component {
                         <button id="communitySuggestion">In a hurry? See other results</button>
                     </div>
                 </form>
-
-
             </header>
         )
     }
