fb329e5e7096b1f3958c2d3542f965bde3bdcabc
Alice-Lau <alice.p.k.lau@gmail.com>
2019-06-08 08:23:27 -0400

    firebase function: store, retrieve to DOM, delete from DOM

--- src/component/SavedCombos.js
+++ src/component/SavedCombos.js
@@ -29,27 +29,46 @@ class SavedCombos extends Component {
                 })
             }
         });
+
+    }
+    
+    removeCombo = (comboId) => {
+        const dbRefCombo = firebase.database().ref(comboId);
+
+        dbRefCombo.remove();
     }
 
     render() {
         return(
-            <div>
+            <div className="combo">
                 <h2>Shared Resutls</h2>
                 {
                     this.state.savedCombos.map((data) => {
-                        {/* const eventImg = data.combo[0].images
-                        console.log(data.combo[0]);
-                        console.log(data.combo[1].restaurant); */}
+                        const eventInfo = data.combo[0];
+                        const eventVenueInfo = eventInfo._embedded.venues[0];
+
+                        const restoInfo = data.combo[1].restaurant;
 
                         return(
                             <div key={data.key}>
                                 <div className="event">
                                     <h2>{data.combo[0].name}</h2>
+                                    <img src={eventInfo.images[0].url} alt={data.combo[0].name}/>
+                                    <p>event type: {eventInfo.classifications[0].segment.name}</p>
+                                    <p>address: {eventVenueInfo.address.line1} {eventVenueInfo.postalCode}</p>
+                                    <a href={eventInfo.url} aria-label="go to ticketmaster page for the current event" target="_blank">book your ticket</a>
                                 </div>
 
                                 <div className="restaurant">
-                                    
+                                    <h2>{restoInfo.name}</h2>
+                                    <img src={restoInfo.featured_image} alt={restoInfo.name}/>
+                                    <p>cuisine type: {restoInfo.cuisines}</p>
+                                    <p>price range: {restoInfo.price_range} / 5</p>
+                                    <p>rating: {restoInfo.user_rating.aggregate_rating} / 5</p>
+                                    <a href={restoInfo.url} aria-label="go to zomato page for more information" target="_blank">more info</a>                                    
                                 </div>
+
+                                <button onClick={() => { this.removeCombo(data.key) }}>remove combo</button>
                             </div>
                         )
                     })
