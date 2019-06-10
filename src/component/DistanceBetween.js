83bdb54cb595bb32d2bead994301c9be8a388da7
helloalexdodd <hello@alexdodd.ca>
2019-06-10 10:38:20 -0400

    working on distance between

--- src/component/DistanceBetween.js
+++ src/component/DistanceBetween.js
@@ -1,16 +1,16 @@
 import React, { Component, Fragment } from 'react';
 
 class DistanceBetween extends Component {
-	componentDidUpdate() {
+	componentDidMount() {
 		this.dataFunction()
 	}
 
 	dataFunction = (restaurant) => {
 		const lat1 = this.props.venueUserInput._embedded.venues[0].location.latitude
 		const lon1 = this.props.venueUserInput._embedded.venues[0].location.longitude
-		// const lat2 = 
-		// const lon2 = 
-		this.distanceBetweenLocations(lat1, lon1)
+		const lat2 = this.props.restaurantData[0].restaurant.location.latitude
+		const lon2 = this.props.restaurantData[0].restaurant.location.longitude
+		this.distanceBetweenLocations(lat1, lon1, lat2, lon2)
 	}
 
 	distanceBetweenLocations = (lat1, lon1, lat2, lon2) => {
@@ -18,12 +18,12 @@ class DistanceBetween extends Component {
 		const equation = 0.5 - Math.cos((lat2 - lat1) * pi) / 2 + Math.cos(lat1 * pi) * Math.cos(lat2 * pi) * (1 - Math.cos((lon2 - lon1) * pi)) / 2;
 		const earth = 6371; //  Earth distance in km so it will return the distance in km
 		const dist = 2 * earth * Math.asin(Math.sqrt(equation));
-		console.log(dist)
+		return dist
 	}
 
 	render() {
 		return (
-			<p>Distance Between</p>
+			<p>{this.distanceBetweenLocations}</p>
 		)
 	}	
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -36,6 +36,8 @@ componentDidMount() {
 	let month = (newMonth < 10) ? "0" + newMonth : newMonth;
 	let day = (newDay < 10) ? "0" + newDay : newDay;
 	let dateString = (`${year}-${month}-${day}T12:00:00Z`)
+	console.log(dateString)
+
 
 	this.setState({ dateString })
 
