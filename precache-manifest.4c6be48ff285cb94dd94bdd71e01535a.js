e1f80f62828cd4fc2d09c734c2d17f0c70674cac
Rushabh Parekh <rushabh.parekh.11@gmail.com>
2019-06-12 15:12:41 -0400

    Updates

---
+++ precache-manifest.4c6be48ff285cb94dd94bdd71e01535a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d66ddb59322e792d5d9f16d327e00c1",
+    "url": "/dinnerAfterShow/index.html"
+  },
+  {
+    "revision": "5c5babb530527fc62b1f",
+    "url": "/dinnerAfterShow/static/css/main.f9b0adeb.chunk.css"
+  },
+  {
+    "revision": "683a25988b16fadd727f",
+    "url": "/dinnerAfterShow/static/js/2.8331d095.chunk.js"
+  },
+  {
+    "revision": "5c5babb530527fc62b1f",
+    "url": "/dinnerAfterShow/static/js/main.76e6f83e.chunk.js"
+  },
+  {
+    "revision": "dbf795dbc682005fff35",
+    "url": "/dinnerAfterShow/static/js/runtime~main.04fcb010.js"
+  },
+  {
+    "revision": "34edbd183286152a5cbcaf62f17d1371",
+    "url": "/dinnerAfterShow/static/media/headerBackground.34edbd18.jpg"
+  },
+  {
+    "revision": "fb5ce5c32b2025f69d056bffb1735df6",
+    "url": "/dinnerAfterShow/static/media/plateSetting.fb5ce5c3.jpg"
+  }
+]);
